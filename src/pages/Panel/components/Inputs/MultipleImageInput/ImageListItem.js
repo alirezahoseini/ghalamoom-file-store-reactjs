9fb1de7ac44ccac0c17fdefd0f392eacf9b659e3
Alirea Hosseini <mehdimj0161@gmail.com>
2023-05-11 17:18:02 +0430

    Image format show in gallery fixed bugs

--- src/pages/Panel/components/Inputs/MultipleImageInput/ImageListItem.js
+++ src/pages/Panel/components/Inputs/MultipleImageInput/ImageListItem.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { TbTrash } from 'react-icons/tb';
 
-export default function ImageListItem({ name, base64, onDeleteFunc, id }) {
-    const [imageInfo, setImageInfo] = useState([])
+export default function ImageListItem({ name, base64, onDeleteFunc, id, type }) {
+    const [imageName, setImageName] = useState([])
+    const [imageType, setImageType] = useState()
     useEffect(() => {
-        let newImageInfoArray = name.split('.');
-        setImageInfo(newImageInfoArray);
+        let newImageName = name.split('.');
+        setImageName(newImageName[0]);
+        let newImageType = type.split('/');
+        setImageType(newImageType[1]);
     }, [])
 
     const deleteButtonHandler = ()=>{
@@ -19,10 +22,10 @@ export default function ImageListItem({ name, base64, onDeleteFunc, id }) {
                 </div>
                 <div className='image-list__body font-bold text-slate-600 w-8/12 flex items-center gap-2 dark:text-slate-100' >
                     <span className='overflow-hidden whitespace-nowrap text-ellipsis'>
-                        {imageInfo[0]}
+                        {imageName}
                     </span>
                     <span className='bg-white text-green-500 font-bold uppercase p-1 pt-2 rounded-md text-xs dark:bg-slate-800'>
-                        {imageInfo[1]}
+                        {imageType}
                     </span>
                 </div>
                 <div onClick={deleteButtonHandler} className="image-list__delete-button ml-auto text-lg  text-slate-500 p-2 rounded-lg cursor-pointer hover:bg-red-100 hover:text-red-600 transition-all duration-100 dark:text-slate-300 dark:hover:text-red-400 dark:hover:bg-red-600 dark:hover:bg-opacity-20">
