2500668278c49f878e1a1cdf4bf77dae2c0357cf
Alirea Hosseini <mehdimj0161@gmail.com>
2023-06-26 00:16:38 +0330

    Add technologies banner

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -4,10 +4,14 @@ import React from 'react'
 // components
 import LandingHeader from './components/LandingHeader/LandingHeader'
 
+//
+import technos from '../../assets/images/landing/technos.svg'
+
 export default function LandingPage() {
   return (
     <div className='container'>
       <LandingHeader />
+      <img src={technos} alt="technos" className='mt-10'/>
     </div>
   )
 }
