07ebb918049851fa8e69e61ef39e5affc3f6b07f
Alirea Hosseini <mehdimj0161@gmail.com>
2023-12-23 22:47:47 +0330

    Change ticket counter boxs style

--- src/pages/Panel/subpages/Tickets/CounterBox.js
+++ src/pages/Panel/subpages/Tickets/CounterBox.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function CounterBox(props) {
   return (
-    <div className="w-6/12 md:w-4/12 xl:w-3/12 p-3" >
+    <div className="w-full md:w-4/12 xl:w-3/12 p-3" >
     <div className=' w-full text-white rounded-xl gap-3 inline-flex items-center justify-start p-2' style={{backgroundColor : props.color}}>
-      <div className="icon text-3xl bg-white bg-opacity-20 p-5 rounded-xl ">
+      <div className="icon text-2xl lg:text-3xl bg-white bg-opacity-20 p-5 rounded-xl ">
         {props.icon}
       </div>
-      <div className="body flex flex-col gap-2">
-        <h2>{props.title}</h2>
-        <span className='text-xl font-bold'>{props.count} عدد</span>
+      <div className="body flex flex-col gap-2 ">
+        <h2 className='text-sm lg:text-sm'>{props.title}</h2>
+        <span className='text-lg lg:text-xl font-bold'>{props.count} عدد</span>
       </div>
     </div>
     </div>
--- src/pages/Panel/subpages/Tickets/NewTicketButton.js
+++ src/pages/Panel/subpages/Tickets/NewTicketButton.js
@@ -4,8 +4,8 @@ import { HiOutlinePlusCircle } from "react-icons/hi2";
 
 function NewTicketButton() {
     return (
-        <div className="w-6/12 md:w-4/12 xl:w-3/12 p-3 " >
-            <div className=' w-full text-white rounded-xl gap-3 inline-flex items-center justify-start p-2 bg-green-500 hover:bg-green-600 hover:cursor-pointer'>
+        <div className="w-full md:w-4/12 xl:w-3/12 p-3 " >
+            <div className=' w-full text-white rounded-xl gap-3 inline-flex items-center justify-center  lg:justify-start p-2 bg-green-500 hover:bg-green-600 hover:cursor-pointer'>
                 <div className="icon text-3xl  p-5 rounded-xl ">
                     <HiOutlinePlusCircle />
                 </div>
